04297632b1bcb368c8dde1320cfb5a6134e712da
Daiki Aminaka <1991.daiki@gmail.com>
2014-10-04 22:06:30 +0900

    implement several frame type using class (refactored)

--- client.py
+++ client.py
@@ -1,29 +1,33 @@
 from pyHPACK.HPACK import encode
-from settings import BaseFlag, CONNECTION_PREFACE
+from settings import BaseFlag, FrameType, CONNECTION_PREFACE
 import sys
-import http2
+from http2 import Connection
 from binascii import hexlify
 from pyHPACK.tables import Table
 import socket
 
-Flag = BaseFlag()
+Flag = BaseFlag
+Type = FrameType
 table = Table()
 sock = None
 
 def access(host, port):
     global sock
+    con = Connection()
+    con.setTable(table)
+
     if not sock:
         sock = socket.create_connection((host, port), 5)
         sock.send(CONNECTION_PREFACE)
         print(hexlify(sock.recv(128)))
-    sock.send(http2.SettingsFrame())
+    sock.send(con.makeFrame(Type.SETTINGS, Flag.NO, 0))
     print(hexlify(sock.recv(128)))
     headers = [[":method", "GET"], [":scheme", "http"], [":authority", "127.0.0.1"], [":path", "/"]]
-    sock.send(http2.HeadersFrame(Flag.NO, headers, 1, table))
-    sock.send(http2.GoAwayFrame(1))
-    a = sock.recv(256)
-    #print(hexlify(a))
-    print(a)
+    con.setHeaders(headers)
+    h = con.makeFrame(Type.HEADERS, Flag.END_HEADERS, 1)
+    sock.send(con.makeFrame(Type.HEADERS, Flag.END_HEADERS, 1))
+    sock.send(con.makeFrame(Type.PING, Flag.NO, 0))
+    sock.send(con.makeFrame(Type.GOAWAY, Flag.NO, 1))
 
 if __name__ == "__main__":
     host = "127.0.0.1"
--- http2.py
+++ http2.py
@@ -1,10 +1,10 @@
 from pyHPACK.HPACK import encode
 from settings import BaseFlag, FrameType, Settings, ErrorCode
 
-Flag = BaseFlag()
-Type = FrameType()
-Set = Settings()
-Err = ErrorCode()
+Flag = BaseFlag
+Type = FrameType
+Set = Settings
+Err = ErrorCode
 
 wireWrapper = lambda x: "".join([chr(int(x[i:i+2], 16)) for i in range(0, len(x), 2)])
 
@@ -13,31 +13,71 @@ def packHex(val, l):
     h = val if type(val) == str else chr(val)
     return "\x00"*(l-len(h)) + h
 
-def HTTP2Frame(length, type, flag, stream_id):
-    return packHex(length, 24) + packHex(type, 8) + packHex(flag, 8) + packHex(stream_id, 32)
+class Connection():
+    def __init__(self, hostType = "client", table = None):
+        self.table = table
+        self.padLen = 0
+        self.hostType = hostType
+        self.lastStream_id = 1 if hostType == "client" else 2 # here dangerous
+        self.streams = [0, self.lastStream_id]
 
-def HeadersFrame(flag, headers, stream_id, table, padLen = 0):
-    frame = ""
-    padding = ""
-    if flag == Flag.PADDED:
-        frame += packHex(padLen, 8) # Pad Length
-        padding = packHex(0, padLen)
-    elif flag == Flag.PRIORITY:
-        # 'E' also should be here
-        frame += packHex(0, 32) # Stream Dependency
-        frame += packHex(0, 8) # Weight
-    frame += wireWrapper(encode(headers, True, True, True, table)) + padding
-    return HTTP2Frame(len(frame), Type.HEADERS, flag, stream_id) + frame
+    def addStream(self):
+        self.lastStream_id = self.streams[-1] + 2
+        self.streams.append(self.lastStream_id)
 
-def SettingsFrame(flag = Flag.NO, stream_id = 0, identifier = Set.NO, value = 0):
-    if flag == Flag.NO:
-        return HTTP2Frame(0, Type.SETTINGS, flag, stream_id)
-    frame = packHex(identifier, 16) + packHex(value, 32)
-    return HTTP2Frame(48, Type.SETTINGS, flag, stream_id) + frame
+    def makeFrame(self, type, flag, stream_id, err = Err.NO_ERROR, debug = None):
+        # here should use **kwargs
+        if type == Type.HEADERS:
+            frame = self._headers(flag)
+        elif type == Type.SETTINGS:
+            frame = self._settings(0)
+        elif type == Type.GOAWAY:
+            frame = self._goAway(err, debug)
+        elif type == Type.PING:
+            frame = self._ping("hello")
+        http2Frame = self.HTTP2Frame(len(frame), type, flag, stream_id)
+        return http2Frame + frame
 
-def GoAwayFrame(lastStream_id, errorCode = Err.NO_ERROR, debugData = ""):
-    # R also should be here
-    frame = packHex(lastStream_id, 32)
-    frame += packHex(errorCode, 32)
-    frame += debugData if debugData else ""
-    return HTTP2Frame(len(frame), Type.GOAWAY, Flag.NO, 0) + frame
+    def HTTP2Frame(self, length, type, flag, stream_id):
+        return packHex(length, 24) + packHex(type, 8) + packHex(flag, 8) + packHex(stream_id, 32)
+
+    def _settings(self, value = 0, flag = Flag.NO, identifier = Set.NO):
+        if flag == Flag.NO:
+            return ""
+        frame = packHex(identifier, 16) + packHex(value, 32)
+        return frame
+
+    def _headers(self, flag):
+        frame = ""
+        padding = ""
+        if flag == Flag.PADDED:
+            frame += packHex(self.padLen, 8) # Pad Length
+            padding = packHex(0, self.padLen)
+        elif flag == Flag.PRIORITY:
+            # 'E' also should be here
+            frame += packHex(0, 32) # Stream Dependency
+            frame += packHex(0, 8) # Weight
+        wire = wireWrapper(encode(self.headers, True, True, True, self.table))
+        # continuation frame should be used if length is ~~ ?
+        frame += wire + padding
+        return frame
+
+    def _goAway(self, err, debug):
+        # R also should be here
+        frame = packHex(self.lastStream_id, 32)
+        frame += packHex(err, 32)
+        frame += debug if debug else ""
+        return frame
+
+    def _continuation(self, wire):
+        # TODO wire length and fin flag should be specified
+        return wire
+
+    def _ping(self, value):
+        return packHex(value, 64)
+
+    def setTable(self, table):
+        self.table = table
+
+    def setHeaders(self, headers):
+        self.headers = headers
