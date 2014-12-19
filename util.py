4a53f57dff73f4f4ce7596c4dd93ebe916965e72
ami-GS <1991.daiki@gmail.com>
2014-12-19 16:20:20 +0900

    fix bug when 'len(val) > l' and 'val >= 256' come

--- util.py
+++ util.py
@@ -1,8 +1,14 @@
 from binascii import hexlify
 
 def packHex(val, l):
-    h = val if type(val) == str else chr(val)
-    return "\x00"*(l-len(h)) + h
+    h = ""
+    if type(val) == str:
+        h += val[:l]
+        h = "\x00"*(l-len(h)) + h
+    else:
+        for i in range(l-1, -1 ,-1):
+            h += chr((val & (0xff << (i * 8))) >> (i * 8))
+    return h
 
 def upackHex(val):
     return int(hexlify(val), 16)
